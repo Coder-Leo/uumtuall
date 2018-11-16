6f52670d230c83342e83e16ff8fb1e9dc249a0fc
Leo <2427755908@qq.com>
2018-11-16 17:59:50 +0800

    实现了item和item_loader的可配置化

--- uumtu/spiders/universal.py
+++ uumtu/spiders/universal.py
@@ -4,7 +4,16 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from uumtu.utils import get_config
 from uumtu.rules import rules
+from uumtu.items import XingganItem
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, Join, Compose
 
+class PicsLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+class XingganLoader(PicsLoader):
+    text_out = Compose(Join(), lambda s: s.strip())
+    source_out = Compose(Join(), lambda s: s.strip())
 
 class UniversalSpider(CrawlSpider):
     name = 'universal'
@@ -20,5 +29,19 @@ class UniversalSpider(CrawlSpider):
 
 
     def parse_item(self, response):
-        i = {}
-        return i
+        item = self.config.get('item')
+        if item:
+            cls = eval(item.get('class'))()
+            loader = eval(item.get('loader'))(cls, response=response)
+            # 动态获取属性配置
+            for key, value in item.get('attrs').items():
+                for extractor in value:
+                    if extractor.get('method') == 'xpath':
+                        loader.add_xpath(key, *extractor.get('args'), **{'re': extractor.get('re')})
+                    if extractor.get('method') == 'css':
+                        loader.add_css(key, *extractor.get('args'), **{'re': extractor.get('re')})
+                    if extractor.get('method') == 'value':
+                        loader.add_value(key, *extractor.get('args'), **{'re': extractor.get('re')})
+                    if extractor.get('method') == 'attr':
+                        loader.add_value(key, getattr(response, *extractor.get('args')))
+        yield loader.load_item()
