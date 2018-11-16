11684dc6991b29a1cb9f56cc5309bc2a6c4ababc
Leo <2427755908@qq.com>
2018-11-16 16:16:04 +0800

    实现了spider基础属性的可配置化

---
+++ uumtu/rules.py
@@ -0,0 +1,9 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule
+
+rules = {
+    'xinggan': (
+        Rule(LinkExtractor(allow='xinggan\/.*\.html',restrict_xpaths='//div[@id="mainbodypul"]//a'), callback='parse_item'),
+        Rule(LinkExtractor(restrict_xpaths='//div[contains(@class, "both")]//a[contains(., "下一页")]'))
+    )
+}
---
+++ uumtu/spiders/universal.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from uumtu.utils import get_config
+from uumtu.rules import rules
+
+
+class UniversalSpider(CrawlSpider):
+    name = 'universal'
+
+    def __init__(self, name, *args, **kwargs):
+        config = get_config(name)
+        print('---- config:', config)
+        self.config = config
+        self.rules = rules.get(config.get('rules'))
+        self.start_urls = config.get('start_urls')
+        self.allowed_domains = config.get('allowed_domains')
+        super(UniversalSpider, self).__init__(*args, **kwargs)
+
+
+    def parse_item(self, response):
+        i = {}
+        return i
